743e9ab595c57d264819feedc1ad44ed05f1d1d4
fatimamirza94 <38617724+fatimamirza94@users.noreply.github.com>
2018-04-28 13:07:51 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,23 @@ Route::post('addbankBranch','BankbranchController@store')->name('addbankBranch')
 Route::get('branchedit','BankbranchController@edit')->name('branchedit');
 Route::post('branchupdate','BankbranchController@update')->name('branchupdate');
 Route::post('deletebankbranch','BankbranchController@destroy');
+
+//DIVISION ROUTING
+Route::get('division','DivisionController@index')->name('division');
+Route::post('addDivision','DivisionController@store');
+Route::post('editDivision','DivisionController@update');
+Route::post('deleteDivision','DivisionController@destroy');
+
+//DISTRICT ROUTING
+Route::get('district','DistrictController@index')->name('district');
+Route::post('addDistrict','DistrictController@store')->name('addDistrict');
+Route::get('districtedit','DistrictController@edit')->name('districtedit');
+Route::post('editDistrict','DistrictController@update')->name('editDistrict');
+Route::post('deleteDistrict','DistrictController@destroy');
+
+//THANA ROUTING
+Route::get('thana','ThanaController@index')->name('thana');
+Route::post('addThana','ThanaController@store')->name('addThana');
+Route::get('thanaedit','ThanaController@edit')->name('thanaedit');
+Route::post('editThana','ThanaController@update')->name('editThana');
+Route::post('deleteThana','ThanaController@destroy');
