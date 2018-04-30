6978a4cd70b9e8b9c106750721603613e2f3cabf
fatimamirza94 <38617724+fatimamirza94@users.noreply.github.com>
2018-05-01 01:19:47 +0600

    Update Atm.php

--- app/Atm.php
+++ app/Atm.php
@@ -1,38 +1,78 @@
 <?php
 
-namespace App;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 use App\District;
 use App\Thana;
 use App\Bank;
 use App\Atm;
-class Atm extends Model
-{    
-    protected $appends = ['district_name','thana_name','bank_name'];
-     protected $fillable = ['district_id','thana_id','bank_id','address'];
+use Illuminate\Support\Facades\Input;
+use App\http\Requests;
+class AtmController extends Controller
+{
+   public function index(){
+   		$atm=Atm::all();
+        $atmdistrict=District::all();
+		$atmthana=Thana::all();//get data from table
+		$atmbank=Bank::all();
+		return view('MasterForms.atmlist',compact('atm','atmthana','atmdistrict','atmbank'));//sent data to view
 
-     public function district()
-    {
-    	return $this->belongsTo(District::class);
-    }
-    public function getDistrictNameAttribute(){
-         return $this->district->name;
-    }
+	} 
+ /*public function atmfunct(){
+        $atmdistrict=District::all();
+		$atmthana=Thana::all();//get data from table
+		return view('MasterForms.atmlist',compact('atmthana','atmdistrict'));//sent data to view
 
-     public function thana()
-    {
-    	return $this->belongsTo(Thana::class);
-    }
-    public function getThanaNameAttribute(){
-         return $this->thana->name;
-    }
+	}*/
+	public function findThanaName(Request $request){
 
-     public function bank()
+		
+	    //if our chosen id and products table prod_cat_id col match the get first 100 data 
+
+        //$request->id here is the id of our chosen option id
+        $data=Thana::select('name','id','district_id')->where('district_id',$request->id)->take(100)->get();
+        return response()->json($data);//then sent this data to ajax success
+	}
+    
+     public function store(Request $request)
     {
-    	return $this->belongsTo(Bank::class);
+    	if($request->ajax())
+        {
+            $atm = Atm::create($request->all());
+            return response($atm);
+        }
+     }
+
+   public function edit(Request $request)
+    {
+        if($request->ajax())
+        {
+            $atm = Atm::find($request->id);
+            $atm->update($request->all());
+            return response($atm);
+        }
     }
-    public function getBankNameAttribute(){
-         return $this->bank->name;
+    public function update(Request $request)
+    {
+        if($request->ajax())
+        {
+            $atm = Atm::find($request->id);
+            $atm->update($request->all());
+            return response($atm);
+        }
     }
+    /*public function find($id)
+    {
+        return Bankbranch::join('banks','banks.id','=','bankbranches.bank_id')->selectRaw('banks.name','bankbranches.id')->where('bankbranches,id',$id)->bank();
+    }*/
+       public function destroy(Request $request)
+    {
+       $atm = Atm::findOrFail($request->id);
+       $atm->delete();
+
+       // $atm = Atm::find ($request->id)->delete();
+        return response()->json();
+    }
+        
 }
